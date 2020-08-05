1231a19c5a137b9ed7909f60166bf3d51173f278
AndrewZ27 <zabolotckiy@gmail.com>
2020-08-06 00:03:53 +0300

    почти сделали дз 8

--- src/index.js
+++ src/index.js
@@ -1,31 +1,48 @@
 import galleryItem from './gallery-items.js';
 
-
-
-let gallery = document.querySelector('.gallery');
+const refs = {
+	gallery: document.querySelector('.gallery'),
+	ulGallery: document.querySelector('.js-gallery'),
+	modalOpen: document.querySelector('.js-lightbox'),
+	modalClose: document.querySelector('.lightbox__button'),
+	lightBox: document.querySelector('.lightbox__image'),
+	lightBoxOverlay: document.querySelector('.lightbox__content')
+};
 
 galleryItem.map((el) => {
-	gallery.insertAdjacentHTML(
+	refs.gallery.insertAdjacentHTML(
 		'beforeend',
-        `<li class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></li>`
+		`<li class="gallery__item"><a class="gallery__link" href=${el.original}><img class="gallery__image" src=${el.preview} data-source=${el.original} alt=${el.description}/></a></li>`
 	);
 });
 
+refs.ulGallery.addEventListener('click', (event) => {
+	event.preventDefault();
 
+	refs.modalOpen.classList.add('is-open');
+	refs.lightBox.src = event.target.getAttribute('data-source');
 
+	console.log(`ссылка на большую фотку ${event.target.getAttribute('data-source')}`);
+});
 
-const ulGallery = document.querySelector(".js-gallery")
-ulGallery.addEventListener('click', event => {
-    event.preventDefault()
-    if (event.target === 'IMG') {
-        
-    }
-    console.log(event.target );
-} )
+refs.modalClose.addEventListener('click', (event) => {
+	refs.modalOpen.classList.remove('is-open');
+});
 
+refs.lightBoxOverlay.addEventListener('click', (event) => {
+	refs.modalOpen.classList.remove('is-open');
 
-const btnPlusOne = document.querySelector("[data-action=increment]");
+	console.log(`got it`);
+});
 
+// const ulGallery = document.querySelector('.js-gallery');
 
+// const lightBox = document.querySelector('.lightbox__image');
+// console.log(lightBox);
 
+// const modalOpen = document.querySelector('.js-lightbox');
 
+// const gallery = document.querySelector('.gallery');
+// const modalClose = document.querySelector('.lightbox__button');
+
+// console.log(modalClose);
